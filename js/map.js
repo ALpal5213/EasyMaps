de178a7a03f54c86ef4e459933155b9d67482263
Adrian Lewis <lewisadrian5213@gmail.com>
2022-09-30 19:49:53 -0600

    ver 1.5

--- js/map.js
+++ js/map.js
@@ -1,16 +1,21 @@
 /*
 Author: Adrian J. Lewis
-Date: 18 Sept 2022
-JavaScript used to calculate distance
+Date: 30 Sept 2022
+JavaScript used to calculate distance, 
+direction, receive device location, and 
+reset the website
 */
+
 var lat;
 var lon;
 var update;
 
+/*Reset webpage*/
 function reset() {
 	document.location.reload(true);
 }
 
+/*Determine distance and direction from user input*/
 function map() {
 	/*Get input data from webpage, c prefix stands for current, 
 	d prefix stands for destination*/
@@ -19,7 +24,7 @@ function map() {
 	var dlat = document.getElementById("dlat").value;
 	var dlon = document.getElementById("dlon").value;
 
-	/*conversion ratio between degrees <-> radians*/
+	/*conversion ratio between degrees and radians*/
 	var convert = Math.PI / 180.0;
 
 	/*Angles for calculations converted to radians*/
@@ -36,6 +41,8 @@ function map() {
 	var direction = calcDirection(B, X, a, distance[1], convert);
 	document.getElementById("direction").innerHTML = direction + "\u00B0";
 
+	/*Ask for device location every 5 seconds only
+	if the user selected device location for input*/
 	if (update == 0) {
 		update = 1;
 		setTimeout(deviceLocation, 5000);
@@ -60,9 +67,10 @@ function calcDirection(B, X, a, b, convert) {
 	var A0;
 	var A;
 
+	/*Calculate A0 and turn into degrees*/
 	A0 = Math.asin(Math.sin(Math.abs(B)) * Math.sin(a) / Math.sin(b)) / convert;
 
-	
+	/*Determine by "inspection" direction of A*/
 	if (B <= 0 && X <= 0) {       /*if destination is east and north*/
 		A = A0;
 	} else if (B > 0 && X <= 0) { /*if destination is west and north*/
@@ -71,22 +79,26 @@ function calcDirection(B, X, a, b, convert) {
 		A = 180.0 - A0;
 	} else if (B >= 0 && X > 0) { /*if destination is west and south*/
 		A = 180.0 + A0;
-	} else {
+	} else {                      /*Should not go here*/
 		A = A0;
-		console.log("error")      /*Should not go here*/
+		console.log("error")      /*Used to debug if direction is incorrect*/
 	}
 	
 	return A.toFixed(2);
 }
 
+/*Calls Geolocation API*/
 function deviceLocation() {
   	navigator.geolocation.getCurrentPosition(getPosition);
+
+  	/*Only enter if map() calls to update*/
   	if (update == 1) {
   		setTimeout(map, 0);
   	}
   	update = 0;
 }
 
+/*Sets position to device's latitude and longitude coordinates*/
 function getPosition(position) {
 	document.getElementById("clat").value = position.coords.latitude;
   	document.getElementById("clon").value = position.coords.longitude;
